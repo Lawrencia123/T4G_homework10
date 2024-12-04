44dc38fb1a1311ee1cb39ef66f16d68061e96020
Lawrencia Baaba Mensah <lawbabmensah@gmail.com>
2024-12-04 14:19:30 +0000

    added new files

---
+++ src/index.js
@@ -0,0 +1,25 @@
+let dom = document.getElementById("dom");
+dom.style.backgroundColor = "gray";
+dom.style.color = "black";
+dom.innerHTML = "I know now what the DOM is 🙌 ";
+
+let para2 = document.getElementById("para2");
+para2.style.backgroundColor = "yellow";
+para2.style.color ="black"
+para2.innerHTML = "I now know how to access / select elements in the DOM 👊🏼"
+
+let para3 = document.getElementById("para3");
+para3.style.backgroundColor = "green";
+para3.style.color = "black";
+para3.innerHTML = "I know how to use the style methods to change the stle of the element in the DOM ✔️";
+
+let para4 = document.getElementById("para4");
+para4.style.backgroundColor = "blueviolet";
+para4.style.color = "white";
+para4.innerHTML = "I know how the styling properties differ in JS from CSS 👍 "
+
+
+let para5 = document.getElementById("para5");
+para5.style.backgroundColor = "black";
+para5.style.color = "white";
+para5.innerHTML = "Yes i did it! I manipulated the DOM and styled my elemets 🤩🤩🤩🤩"
